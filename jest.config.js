e72ffb3744ad5968e77653a78d9cd58131acb0a3
MariamMohammed99 <mariammamin19@gmail.com>
2024-02-17 00:38:27 +0200

    feat: packages installed and config is set

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(scss|sass|css)$': 'identity-obj-proxy',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.json',
+    },
+  },
+  setupFilesAfterEnv: ['./src/jest.setup.ts'],
+}
