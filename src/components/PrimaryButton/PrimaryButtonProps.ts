940e4830249e079be53dfb755b6df2859d325651
MariamMohammed99 <mariammamin19@gmail.com>
2024-02-18 12:01:21 +0200

    feat: add reusable button

---
+++ src/components/PrimaryButton/PrimaryButtonProps.ts
@@ -0,0 +1,8 @@
+export default interface PrimaryButtonProps {
+  text: string;
+  onClick: () => void;
+  isDisabled?: boolean;
+  color?: string;
+  borderColor?: string;
+  textColor?: string;
+}
--- src/constants/appConstants.ts
+++ src/constants/appConstants.ts
@@ -2,3 +2,8 @@
 export const baseUrl = 'https://plotter-task-8019e13a60ac.herokuapp.com/';
 export const columnsUrl = 'columns';
 export const dataUrl = 'data';
+
+// Color Constants
+export const buttonColor = '#F7A116';
+export const buttonBorderColor = '#C27800';
+export const buttonTextColor = '#fff';
